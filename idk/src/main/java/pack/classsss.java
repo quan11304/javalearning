4fb5dca18772c918430597370067213a4cc66eeb
Quan Hoang <quan11304@gmail.com>
2022-08-24 21:40:04 +0700

    2

--- idk/src/main/java/pack/classsss.java
+++ idk/src/main/java/pack/classsss.java
@@ -1,25 +1,14 @@
 package pack;
+import java.util.Scanner;
 
 public class classsss {
     public static void main(String[] args) {
-        int number = 2;
-        switch(number){
-            case 1:
-                System.out.println("One");
-                break;
-            case 2:
-                System.out.println("Two");
-            case 3:
-                System.out.println("Three");
-                break;
-            case 4:
-                System.out.println("Four");
-                break;
-            case 5:
-                System.out.println("Five");
-                break;
-            default:
-                System.out.println("Không có lựa chọn này");
-        }
+        System.out.print("Index: ");
+
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+
+        String string = "ASCII";
+        System.out.printf("\nCharacter in string %s at index #%i",string,i,string.charAt(i-1));
     }
 }
