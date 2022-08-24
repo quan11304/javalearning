0e58a72c22c829707f2683dac88934be781c3f8f
Quan Hoang <quan11304@gmail.com>
2022-08-24 20:03:36 +0700

    idk

---
+++ idk/src/main/java/pack/classsss.java
@@ -0,0 +1,25 @@
+package pack;
+
+public class classsss {
+    public static void main(String[] args) {
+        int number = 2;
+        switch(number){
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+            case 3:
+                System.out.println("Three");
+                break;
+            case 4:
+                System.out.println("Four");
+                break;
+            case 5:
+                System.out.println("Five");
+                break;
+            default:
+                System.out.println("Không có lựa chọn này");
+        }
+    }
+}
